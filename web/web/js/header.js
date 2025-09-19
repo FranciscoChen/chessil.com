6fd1ee32ee2f1f4b5ab5bd8f5feffd09bbf3287a
Kico Chen <flamerare@gmail.com>
2025-09-19 03:09:53 +0200

    remove seeks

--- web/web/js/header.js
+++ web/web/js/header.js
@@ -39,7 +39,7 @@ function elementOrAncestorHasId(element, idName) {
 
 function headerfunctions() {
   settings = document.getElementsByClassName("dasher")[0];
-  seeks = document.getElementsByClassName("seek")[0];
+
   userbutton = document.getElementById("user_button");
   dasher = document.getElementById("dasher_app");
   clinput = document.getElementById("clinput");
@@ -72,160 +72,7 @@ function headerfunctions() {
       setlightmode()
     }
   }
-  noresults = { en: 'No results.', es: 'No hay resultados.', zh: '没有结果' }[lang]
-  nochallenges = { en: 'No challenges.', es: 'No hay retos.', zh: '没有挑战' }[lang]
-  seek = document.getElementsByClassName('seek')[0]
-  const challenges = document.createElement('div')
-  challenges.setAttribute('class', 'challenges')
-  const challengeapp = document.getElementById('challenge-app')
-  const rated = { en: 'Rated', es: 'Clasificatoria', zh: '排位' }[lang]
-  const casual = { en: 'Casual', es: 'Amistosa', zh: '友谊' }[lang]
-  const newgame = { en: 'New game', es: 'Nueva partida', zh: '新局' }[lang]
-  const classical = { en: 'Classical', es: 'Clásica', zh: '慢棋' }[lang]
-  const rapid = { en: 'Rapid', es: 'Rápida', zh: '快棋' }[lang]
-  const blitz = { en: 'Blitz', es: 'Relámpago', zh: '闪电' }[lang]
-  const bullet = { en: 'Bullet', es: 'Bala', zh: '子弹' }[lang]
-  const ultrabullet = { en: 'Ultrabullet', es: 'Ultrabala', zh: '超弹' }[lang]
-  const minsperside = { en: 'Minutes per side: ', es: 'Minutos por jugador: ', zh: '各方限时（分钟）：' }[lang]
-  const incrinseconds = { en: 'Increment in seconds: ', es: 'Incremento en segundos: ', zh: '每步加时（秒）：' }[lang]
 
-  function getseeks() {
-    if (document.visibilityState !== "visible") return
-    var nchallenges = 0
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", '/getseeks', true);
-    xhr.send()
-    xhr.onreadystatechange = function () {
-      if (this.readyState != 4) return;
-      if (this.status == 200) {
-        const see = JSON.parse(this.responseText)
-        newseeklist = []
-        //console.log(this.responseText)
-        const l = see.length
-        if (l === 0) {
-          challengeapp.innerHTML = '<div class="empty text">' + nochallenges + '</div>'
-        } else {
-          if (document.getElementsByClassName('empty text').length === 1) {
-            challengeapp.removeChild(document.getElementsByClassName('empty text')[0])
-          }
-          if (document.getElementsByClassName('challenges').length === 0) {
-            challengeapp.appendChild(challenges)
-          }
-        }
-        //challengeapp.innerHTML = ''
-        //challenges.innerHTML = ''
-        // List all new results
-        for (var i = 0; i < l; ++i) {
-          const gs = see[i]
-          if (gs.target != null && document.getElementById('user_tag') != null && gs.target == document.getElementById('user_tag').textContent) ++nchallenges;
-          newseeklist.push(gs.seekid)
-          if (seeklist.indexOf(gs.seekid) === -1) {
-            seeklist.push(gs.seekid)
-            const ne = document.createElement('div')
-            ne.setAttribute('id', gs.seekid)
-            var ih = '<div class="content"><div.content__text>'
-            var clist = 'challenge in'
-            var colorside = { null: 'random', true: 'white', false: 'black' }[gs.side]
-            if (gs.target != null) {
-              // Is a Challenge
-              if (document.getElementById('user_tag') != null && gs.target == document.getElementById('user_tag').textContent) {
-                // User is being targeted, display opponent or anon (NN)
-                if (gs.username == null) {
-                  ih += '<span class="head"><a class="user-link online"><i class="line"></i><name>NN</name></a></span>'
-                } else {
-                  ih += '<span class="head"><a class="user-link online" href="/@/' + gs.username + '"><i class="line"></i><name>' + gs.username + '</name></a></span>'
-                }
-                colorside = { random: 'random', black: 'white', white: 'black' }[colorside]
-              } else {
-                ih += '<span class="head"><a class="user-link online" href="/@/' + gs.target + '"><i class="line"></i><name>' + gs.target + '</name></a></span>'
-                clist += ' waiting'
-              }
-            } else {
-              // Is a pool seek
-              clist += ' seeking'
-            }
-
-            ih += '<span class="desc"><span class="is color-icon ' + colorside + '"></span> • ' + { true: rated, false: casual }[gs.rated] + ' • ' + gs.initialtime + '+' + gs.increment + '</span></div.content__text></div>'
-            ne.setAttribute('class', clist)
-            ne.innerHTML = ih
-            // FROM HERE IS BUTTONS
-            const btns = document.createElement('div')
-            btns.setAttribute('class', 'buttons')
-            // ih += '<div class="buttons">'
-            // If target is null,  there is no accept
-            if (gs.target == null) {
-              //ih += '<button class="button seek" data-icon="'+seekicon+'"></button>'
-              const btseek = document.createElement('button')
-              btseek.setAttribute('class', 'button seek')
-              btseek.setAttribute('data-icon', seekicon)
-              btns.appendChild(btseek)
-            } else {
-              // If there is a target, and it's me, there is accept 
-              if (document.getElementById('user_tag') != null && gs.target == document.getElementById('user_tag').textContent) {
-                //ih += '<button class="button accept" tc="'+gs.initialtime+'+'+gs.increment+{true:'r',false:'u'}[gs.rated]+{null:'r',true:'b',false:'w'}[gs.side]+'" data-icon="'+accepticon+'"></button>'
-                const btac = document.createElement('button')
-                btac.setAttribute('class', 'button accept')
-                btac.setAttribute('data-icon', accepticon)
-                btac.onclick = function () {
-                  acceptedchallenge(gs.initialtime + '+' + gs.increment + { true: 'r', false: 'u' }[gs.rated] + { null: 'r', true: 'b', false: 'w' }[gs.side] + ':' + gs.seekid)
-                }
-                btns.appendChild(btac)
-              } else {
-                //ih += '<button class="button seek" data-icon="'+seekicon+'"></button>'
-                const btseek = document.createElement('button')
-                btseek.setAttribute('class', 'button seek')
-                btseek.setAttribute('data-icon', seekicon)
-                btns.appendChild(btseek)
-              }
-            }
-
-            //ih += '<button class="button decline" data-icon="'+declineicon+'" </button></div>'
-            const btde = document.createElement('button')
-            btde.setAttribute('class', 'button decline')
-            btde.setAttribute('data-icon', declineicon)
-            btde.onclick = function () {
-              decline(this.parentElement.parentElement.id)
-              setTimeout(() => {
-                challenges.removeChild(this.parentElement.parentElement)
-                if (challenges.children.length === 0) challengeapp.innerHTML = '<div class="empty text">' + nochallenges + '</div>'
-              }, 100)
-            }
-            btns.appendChild(btde)
-
-            ne.appendChild(btns)
-            challenges.appendChild(ne)
-          }
-        }
-        // Clear previous results that are not listed anymore
-        for (var i = seeklist.length; i--;) {
-          const pr = document.getElementById(seeklist[i])
-          if (pr !== null && newseeklist.indexOf(seeklist[i]) === -1) {
-            challenges.removeChild(pr)
-          }
-        }
-        // Update number of challenges
-        document.getElementsByClassName('data-count')[0].setAttribute('data-count', nchallenges)
-      }
-    }
-  }
-
-  setInterval(pairseeks, 4000)
-  setInterval(intervalgetseeks, 7000)
-  function intervalgetseeks() {
-    if (document.getElementById('user_tag') != null) getseeks()
-  }
-  function pairseeks() {
-    if (document.visibilityState === "visible") {
-      const sks = document.getElementsByClassName('seeking')
-      for (var i = sks.length; i--;) {
-        pair(sks[i].id)
-      }
-      const chl = document.getElementsByClassName('waiting')
-      for (var i = chl.length; i--;) {
-        startchallenge(chl[i].id)
-      }
-    }
-  }
 
   si.addEventListener("input", function () {
     clearTimeout(tto);
@@ -297,114 +144,7 @@ function headerfunctions() {
     }
     document.head.appendChild(link);
   }
-  function decline(seekid) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", '/cancel', true);
-    xhr.send(seekid)
-  }
-  function pair(seekid) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", '/pairing', true);
-    xhr.send(seekid)
-    xhr.onreadystatechange = function () {
-      if (this.readyState != 4) return;
-      if (this.status == 202) {
-        document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('blue')
-        setTimeout(() => {
-          if (document.getElementById(seekid) != null) {
-            document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('red');
-            document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('blue')
-          }
-        }, 1000)
-        setTimeout(() => {
-          if (document.getElementById(seekid) != null) {
-            document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('yellow')
-            document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('red');
-          }
-        }, 2000)
-        setTimeout(() => {
-          if (document.getElementById(seekid) != null) {
-            document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('orange');
-            document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('yellow')
-          }
-        }, 3000)
-        setTimeout(() => {
-          if (document.getElementById(seekid) != null) {
-            document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('orange');
-          }
-        }, 4000)
-      }
-      if (this.status == 201) {
-        window.location.href = this.responseText
-      }
-    }
-  }
-  function acceptedchallenge(timecontrolwithseekid) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", '/accept', true);
-    xhr.send(timecontrolwithseekid)
-    xhr.onreadystatechange = function () {
-      if (this.readyState != 4) return;
-      if (this.status == 200) {
-        // console.log(this.responseText)
-        startchallenge(this.responseText)
-      }
-    };
-  }
 
-  function startchallenge(seekid) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", '/startchallenge', true);
-    xhr.send(seekid)
-    xhr.onreadystatechange = function () {
-      if (this.readyState != 4) return;
-      if (this.status == 202) {
-        document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('blue')
-        setTimeout(() => {
-          if (document.getElementById(seekid) != null) {
-            document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('red');
-            document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('blue')
-          }
-        }, 1000)
-        setTimeout(() => {
-          if (document.getElementById(seekid) != null) {
-            document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('yellow')
-            document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('red');
-          }
-        }, 2000)
-        setTimeout(() => {
-          if (document.getElementById(seekid) != null) {
-            document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('orange');
-            document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('yellow')
-          }
-        }, 3000)
-        setTimeout(() => {
-          if (document.getElementById(seekid) != null) {
-            document.getElementById(seekid).getElementsByClassName('seek')[0].classList.toggle('orange');
-          }
-        }, 4000)
-      }
-      if (this.status == 201) {
-        window.location.href = this.responseText
-      }
-    }
-  }
-  /*
-function trypairing(seekid, retry = 0) {
-var xhr = new XMLHttpRequest();
-xhr.open("POST", '/pairing', true);
-xhr.send(seekid)
-xhr.onreadystatechange = function () {
-if (this.readyState != 4) return;
-if (this.status == 202) {
-  setTimeout( ()=>{trypairing(seekid, retry)}, 2000 )
-}
-if (this.status == 201) {
-  window.location.href = this.responseText
-}
-}
-}
-*/
   document.onclick = (e) => {
     if (elementOrAncestorHasClass(e.target, 'dasher')) {
       if (e.target.className.indexOf('toggle') > -1 || e.target.parentElement.className.indexOf('toggle') > -1) {
@@ -462,7 +202,6 @@ if (this.status == 201) {
     }
   }
 
-  getseeks()
 }
 
 function isloggedin() {
